04ddfd1e8a0a56133dcb067ce35ec9ba301b0620
Shubham Attri <shubham21354@iiitd.ac.in>
2023-06-17 22:20:44 +0530

    Fixed Background 2023
    Added some more files as well for work, will repair nav bar next

---
+++ src/components/Menu.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Menu.css';
+
+const Menu = () => {
+    return (
+        <div className="menu">
+            <h1>Menu</h1>
+        </div>
+    );
+};
+export default Menu;
