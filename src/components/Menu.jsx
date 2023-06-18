7ed7090176bcbd03df9c8ba938b74195541b0990
Shubham Attri <shubham21354@iiitd.ac.in>
2023-06-18 18:22:23 +0530

    imported logos

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import './Menu.css';
+import inductionlogo from '../components/Assets/InductionLogo.png';
+import iiitdlogo from '../components/Assets/iiitd white logo 2.png';
+import facebooklogo from '../components/Assets/facebook.png'
+import twitterline from  '../components/Assets/twitter-line.png'
+import instagramline from '../components/Assets/instagram-line.png'
 
 const Menu = () => {
     return (
         <div className="menu">
-            < div className="menu-item1"> Icon Bar
+            < div className="menu-item1">
+                <div className="menu-item1-">
+                    <img  src={iiitdlogo} alt="" />
+                    <img  src={inductionlogo} alt="" />
+                    <img  src={instagramline} alt="" />
+                    <img  src={facebooklogo} alt="" />
+                    <img  src={twitterline} alt="" />
+                </div>
                 <div className="menu-item1-1"> Home </div>
                 <div className="menu-item1-2"> About Us</div>
                 <div className="menu-item1-3"> Location </div>
