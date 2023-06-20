f5ccd5afa55a2cb44b4d13995019972d40358afd
Aditya Girdhar <92713380+adityagirdhar@users.noreply.github.com>
2023-06-21 01:23:46 +0530

    Fixed parallax alignment issue

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import './Landing.css';
 import Navb from './Navb';
+import bg2023 from '../parallaxElems/bg.png'
 import image1 from "../parallaxElems/1.png";
 import image2 from "../parallaxElems/2.png";
 import image3 from "../parallaxElems/3.png";
@@ -31,14 +32,13 @@ const Landing = () => {
 
   return (
     <>
+    <img src={bg2023} alt="2023-background" className='center-panning' />
     <div className='flex justify-center items-center' >
        {/* <p className="absolute text-[#fff] text-[80px] font-[500] text-center pt-[1360px]" data-value="3" >
             30<sup>th</sup> June - 2<sup>nd</sup> July 2023
        </p> */}
     </div>
     <div className="container">
-
-
           <img src={image1} className="object" data-value="-2" alt="" />
           <img src={image2} className="object" data-value="6" alt="" />
           <img src={image3} className="object" data-value="4" alt="" />
