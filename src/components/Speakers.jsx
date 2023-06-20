c461c92f562177538d595911cdc70dd3ad97c3f4
Divyansh <divyansh.72003@gmail.com>
2023-06-20 10:41:59 +0530

    Added animation for slide-in for speakers

--- src/components/Speakers.jsx
+++ src/components/Speakers.jsx
@@ -1,19 +1,34 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react';
+import { useInView } from 'react-intersection-observer';
 import './Speakers.css'
 import speaker from '../components/Assets/Kenote Speakers.png'
 import speakers from '../components/Assets/speaker.png'
 const Speaker = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    rootMargin: '-100px 0px', // Adjust the root margin as needed
+  });
+  const imageRef = useRef(null);
+
+  useEffect(() => {
+    if (inView) {
+      setIsVisible(true);
+    }
+  }, [inView]);
+
   return (
     <div className='speak'>
         <img src = {speaker} className={"welc"}  alt="welcome" />
-        {/* <div class='container'> */}
   
-          <div class="slide-right pl-[140px]">
-           <img src={speakers} alt="" />
-  {/* </div> */}
-
-  
-</div>
+        <div ref={ref} className={`${isVisible ? "slide-right" : ''}`}>
+      <img
+        ref={imageRef}
+        src={speakers}
+        alt="Your Image"
+        className='pl-[100px]'
+      />
+    </div>
     </div>
   )
 }
