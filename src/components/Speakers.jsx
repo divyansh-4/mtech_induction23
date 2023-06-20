55b3d744f740ba3215ed75edc6c425c369026cf1
Divyansh <divyansh.72003@gmail.com>
2023-06-20 10:22:22 +0530

    Added Speakers with animations

--- src/components/Speakers.jsx
+++ src/components/Speakers.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import './Speakers.css'
 import speaker from '../components/Assets/Kenote Speakers.png'
-
+import speakers from '../components/Assets/speaker.png'
 const Speaker = () => {
   return (
     <div className='speak'>
         <img src = {speaker} className={"welc"}  alt="welcome" />
+        {/* <div class='container'> */}
+  
+          <div class="slide-right pl-[140px]">
+           <img src={speakers} alt="" />
+  {/* </div> */}
+
+  
+</div>
     </div>
   )
 }
