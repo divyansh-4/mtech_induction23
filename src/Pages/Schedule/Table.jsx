793e7f2dc86548cdbaae2fbe1b51a1a97f4eb9f5
Shubham Attri <shubham21354@iiitd.ac.in>
2023-06-27 07:13:56 +0530

    added styles

--- src/Pages/Schedule/Table.jsx
+++ src/Pages/Schedule/Table.jsx
@@ -1,28 +1,35 @@
 import React from 'react';
 import './Table.css'
+import P from '../Pages/bgassets/P.svg'
+import H from '../Pages/bgassets/H.svg'
+import M from '../Pages/bgassets/M.svg'
+import T from '../Pages/bgassets/T.svg'
+import D from '../Pages/bgassets/D.svg'
 
 const Table = ({ sched }) => {
   return (
-    <table className="tableclass ml-[50px]">
-      <thead>
-        <tr>
-          <th></th>
-          <th></th>
-        </tr>
-      </thead>
+    <div className="tableclass">
+      <table className="tableclass">
       <tbody>
         {sched.map((day, index) => {
           const { time, event } = day;
-
           return (
-            <tr className="sct" key={index}>
-              <td className='leftrow pb-[16px] '>{time}</td>
-              <td className='rightrow   pb-[16px] pl-[80px]'>{event}</td>
+            <tr className="schedule table" key={index}>
+              <td className='leftrow  '>{time}</td>
+              <td className='rightrow '>{event}</td>
             </tr>
           );
         })}
       </tbody>
     </table>
+        <div className="background">
+            <img src={P} className="P" alt="P" />
+            <img src={H} className="H" alt="H" />
+            <img src={M} className="M" alt="M" />
+            <img src={T} className="T" alt="T" />
+            <img src={D} className="D" alt="D" />
+        </div>
+        </div>
   );
 };
 
