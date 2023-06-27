34c66ee5aa9db1176783b0846c5694c751f2b5a9
Shubham Attri <shubham21354@iiitd.ac.in>
2023-06-27 16:06:18 +0530

    Updated Schedule for day 1

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -5,10 +5,17 @@ import H from '../bgassets/H.svg'
 import M from '../bgassets/M.svg'
 import T from '../bgassets/T.svg'
 import D from '../bgassets/D.svg'
+import schedulebg from "../components/Assets/schedule.svg"
+import day1 from "../components/Assets/day1.svg"
+import mainpage from "../components/Assets/mainpage.svg"
 
 const Table = ({ sched }) => {
   return (
     <div className="tableclass">
+        <div className="backgroundimg">
+            <img src={mainpage} className="mainpage" alt="mainpage" />
+            <img src={day1} className="day1" alt="day1" />
+        </div>
       <table className="tableclass">
       <tbody>
         {sched.map((day, index) => {
